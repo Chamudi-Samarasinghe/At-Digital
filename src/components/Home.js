c05b5f5a5a2ac655cc8413741bbbc37c72972b4a
Chamudi-Samarasinghe <ctsamarasinghe@students.nsbm.ac.lk>
2024-04-22 07:29:13 +0530

    change

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,20 +9,6 @@ import image1 from "../components/images/1.png";
 import image2 from "../components/images/2.png";
 import "./Home.css";
 
-// var acc = document.getElementsByClassName("accordion");
-//   var i;
-  
-//   for (i = 0; i < acc.length; i++) {
-//       acc[i].addEventListener("click", function () {
-//           this.classList.toggle("active");
-//           var panel = this.nextElementSibling;
-//           if (panel.style.maxHeight) {
-//               panel.style.maxHeight = null;
-//           } else {
-//               panel.style.maxHeight = panel.scrollHeight + "px";
-//           }
-//       });
-//   }
 
 
 function Home() {
